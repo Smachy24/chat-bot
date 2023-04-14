2fb792132a917a874690bdddb6d275d8a2f3f6b4
Tartuffe-coding <khaled.kouadria@edu.esiee-it.fr>
2023-04-14 13:59:32 +0200

    server code documentation

--- ServerTCP/Logger.cs
+++ ServerTCP/Logger.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 namespace ServerTCP
 {
+	/// <summary>
+	/// Objet nous permettant d'écrire un message dans un fichier un log, il prend en compte la gestion de l'heure du message et son formatage.
+	/// </summary>
 	public class Logger
 	{
         private static readonly string _logFileName = "LogClass.txt";
@@ -9,6 +12,10 @@ namespace ServerTCP
 		private string _logMessage;
 		StreamWriter LogWriter = new StreamWriter(_logFileName, true);
 
+		/// <summary>
+		/// A l'instantiation, le Logger Recupère notre message et la date d'instantiation puis l'écrit directement dans le fichier de log.
+		/// </summary>
+		/// <param name="message">le corp du message du log</param>
         public Logger(string message)
 		{
 			_logDateTime = DateTime.Now;
@@ -20,7 +27,6 @@ namespace ServerTCP
 			LogWriter.Close();
 		}
 
-		//public void purgeLo  // faire une fonction pour purger les log
     }
 }
 
--- ServerTCP/User.cs
+++ ServerTCP/User.cs
@@ -7,16 +7,25 @@ using System.Threading.Tasks;
 
 namespace ServerTCP
 {
+    /// <summary>
+    /// Type construit pour representer chaque nouveau socket client a un numéro d'index et un pseudo
+    /// </summary>
     public class User
     {
         public Socket Socket { get; set; }
         public int Number { get; set; }
         public string Pseudo { get; set; }
 
+        /// <summary>
+        /// Construit un nouveau client. Son pseudo n'est pas encore définie.
+        /// </summary>
+        /// <param name="s">le socket de ce nouveu client</param>
         public User(Socket s, int num)
         {
             Socket = s;
             Number = num;
+            // a la construction le pseudo est initialiser a une chaine vide, cela nous permettra d'identifier les client nouvelle connecté
+            // pour qu'il puisse saisir leur pseudo.
             Pseudo = String.Empty;
         }
     }
